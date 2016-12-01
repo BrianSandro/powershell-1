6b38e2f76fa13caa2e7cf9e791609e6a96fd06b1
David <blastikman@gmail.com>
2016-12-01 11:54:38 +0100

    Update get-lockedout-user.ps1

--- get-lockedout-user.ps1
+++ get-lockedout-user.ps1
@@ -1,30 +1,3 @@
-<#
-.SYNOPSIS
-    Get-LockedOutUser.ps1 returns a list of users who were locked out in Active Directory.
- 
-.DESCRIPTION
-    Get-LockedOutUser.ps1 is an advanced script that returns a list of users who were locked out in Active Directory
-by querying the event logs on the PDC emulation in the domain.
- 
-.PARAMETER UserName
-    The userid of the specific user you are looking for lockouts for. The default is all locked out users.
- 
-.PARAMETER StartTime
-    The datetime to start searching from. The default is all datetimes that exist in the event logs.
- 
-.EXAMPLE
-    Get-LockedOutUser.ps1
- 
-.EXAMPLE
-    Get-LockedOutUser.ps1 -UserName 'mike'
- 
-.EXAMPLE
-    Get-LockedOutUser.ps1 -StartTime (Get-Date).AddDays(-1)
- 
-.EXAMPLE
-    Get-LockedOutUser.ps1 -UserName 'miker' -StartTime (Get-Date).AddDays(-1)
-#>
-
 [CmdletBinding()]
 param (
     [ValidateNotNullOrEmpty()]
